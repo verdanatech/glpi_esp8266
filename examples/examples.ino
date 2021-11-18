b66e02a94eb48f2a89df9de496bea145f7fe3d38
Marcela Lima <mrl.marcelalima07@gmail.com>
2021-11-18 10:40:21 -0300

    Removing .cpp and .h files from the src folder and deleting the src folder

--- examples/examples.ino
+++ examples/examples.ino
@@ -1,33 +1,33 @@
 #include <glpi_esp8266.h>
- 
-char* HOSTNAME = "verdanadeskIoT"; //Hostname do equipamento na Rede LAN
-char SSID[]   = "..."; //SSID da rede
-char NETKEY[]  = "..."; //Chave de acesso a rede
+
+char *HOSTNAME = "verdanadeskIoT"; //Hostname do equipamento na Rede LAN
+char SSID[] = "...";               //SSID da rede
+char NETKEY[] = "...";             //Chave de acesso a rede
 
 int tentativas = 50;
 
-
-void printNetworkData(){
+void printNetworkData()
+{
 
   Serial.println();
   Serial.println("- - - - - - - -");
-  Serial.println("Hostname: "+ (String)HOSTNAME);
-    
-  Serial.println("Conectado em: "+ (String)SSID);
+  Serial.println("Hostname: " + (String)HOSTNAME);
+
+  Serial.println("Conectado em: " + (String)SSID);
 
   Serial.print("Meu endereço IP: ");
   Serial.println(WiFi.localIP());
 
   Serial.print("MAC: ");
- // Serial.println(WiFi.macAddress());
-  
+  // Serial.println(WiFi.macAddress());
+
   Serial.println("- - - - - - - -");
-  
 }
 
-void wifiConect(){
-  
-/*
+void wifiConect()
+{
+
+  /*
  * Realiza a conexão da esp8266 com a rede wifi
  */
 
@@ -39,28 +39,27 @@ void wifiConect(){
   WiFi.begin(SSID, NETKEY);
 
   // Definindo hostname da placa para LAN
-//  WiFi.hostname(HOSTNAME);
-    
+  //  WiFi.hostname(HOSTNAME);
+
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.print(".");
     tentativas--;
-      
+
     if (tentativas == 0)
     {
       Serial.println();
       Serial.println("Time-out. Verifique as configurações e tente novamente.");
       delay(1000);
-        
+
       //while(true);
     }
-      
   }
-  
 }
 
-void setup(void){
+void setup(void)
+{
 
   //inicialização da comunicação com a porta serial
   Serial.begin(115200);
@@ -69,5 +68,6 @@ void setup(void){
   printNetworkData();
 }
 
-void loop(void){
+void loop(void)
+{
 }
